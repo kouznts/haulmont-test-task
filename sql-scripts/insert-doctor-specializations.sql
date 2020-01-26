ce4bd5a1f77280342bfa5a5afca0f01b70f3d8cc
Andrey <kouznts@ya.ru>
2020-01-26 15:42:06 +0400

    запросы вставки медицинских специальностей

---
+++ sql-scripts/insert-doctor-specializations.sql
@@ -0,0 +1,269 @@
+﻿insert into doctor_specialization (name )
+values ('акушер-гинеколог')
+
+insert into doctor_specialization (name )
+values ('аллерголог-иммунолог')
+
+insert into doctor_specialization (name )
+values ('ангиохирург')
+
+insert into doctor_specialization (name )
+values ('андролог-эндокринолог')
+
+insert into doctor_specialization (name )
+values ('анестезиолог-реаниматолог')
+
+insert into doctor_specialization (name )
+values ('аритмолог')
+
+insert into doctor_specialization (name )
+values ('ароматерапевт')
+
+insert into doctor_specialization (name )
+values ('артролог')
+
+insert into doctor_specialization (name )
+values ('бактериолог')
+
+insert into doctor_specialization (name )
+values ('бальнеолог')
+
+insert into doctor_specialization (name )
+values ('валеолог')
+
+insert into doctor_specialization (name )
+values ('венеролог')
+
+insert into doctor_specialization (name )
+values ('вертебролог')
+
+insert into doctor_specialization (name )
+values ('вирусолог')
+
+insert into doctor_specialization (name )
+values ('врач УЗИ')
+
+insert into doctor_specialization (name )
+values ('врач общей практики')
+
+insert into doctor_specialization (name )
+values ('гастроэнтеролог')
+
+insert into doctor_specialization (name )
+values ('гематолог')
+
+insert into doctor_specialization (name )
+values ('генетик')
+
+insert into doctor_specialization (name )
+values ('гепатолог')
+
+insert into doctor_specialization (name )
+values ('гериатр')
+
+insert into doctor_specialization (name )
+values ('гинеколог')
+
+insert into doctor_specialization (name )
+values ('гирудотерапевт')
+
+insert into doctor_specialization (name )
+values ('дерматовенеролог')
+
+insert into doctor_specialization (name )
+values ('дерматолог')
+
+insert into doctor_specialization (name )
+values ('диагност')
+
+insert into doctor_specialization (name )
+values ('диетолог')
+
+insert into doctor_specialization (name )
+values ('зубной врач')
+
+insert into doctor_specialization (name )
+values ('иглорефлексотерапевт')
+
+insert into doctor_specialization (name )
+values ('иммунолог')
+
+insert into doctor_specialization (name )
+values ('имплантолог')
+
+insert into doctor_specialization (name )
+values ('инфекционист')
+
+insert into doctor_specialization (name )
+values ('кардиолог')
+
+insert into doctor_specialization (name )
+values ('кардиоревматолог')
+
+insert into doctor_specialization (name )
+values ('кардиохирург')
+
+insert into doctor_specialization (name )
+values ('кинезиолог')
+
+insert into doctor_specialization (name )
+values ('колопроктолог')
+
+insert into doctor_specialization (name )
+values ('косметолог')
+
+insert into doctor_specialization (name )
+values ('курортолог')
+
+insert into doctor_specialization (name )
+values ('ЛОР')
+
+insert into doctor_specialization (name )
+values ('лаборант')
+
+insert into doctor_specialization (name )
+values ('логопед')
+
+insert into doctor_specialization (name )
+values ('маммолог')
+
+insert into doctor_specialization (name )
+values ('мануальный терапевт')
+
+insert into doctor_specialization (name )
+values ('массажист')
+
+insert into doctor_specialization (name )
+values ('миколог')
+
+insert into doctor_specialization (name )
+values ('нарколог')
+
+insert into doctor_specialization (name )
+values ('невролог')
+
+insert into doctor_specialization (name )
+values ('невропатолог')
+
+insert into doctor_specialization (name )
+values ('нейротравматолог')
+
+insert into doctor_specialization (name )
+values ('нейрохирург')
+
+insert into doctor_specialization (name )
+values ('нефролог')
+
+insert into doctor_specialization (name )
+values ('окулист')
+
+insert into doctor_specialization (name )
+values ('онкогинеколог')
+
+insert into doctor_specialization (name )
+values ('онколог')
+
+insert into doctor_specialization (name )
+values ('ортодонт')
+
+insert into doctor_specialization (name )
+values ('ортопед')
+
+insert into doctor_specialization (name )
+values ('остеопат')
+
+insert into doctor_specialization (name )
+values ('отоларинголог')
+
+insert into doctor_specialization (name )
+values ('отоневролог')
+
+insert into doctor_specialization (name )
+values ('офтальмолог')
+
+insert into doctor_specialization (name )
+values ('паразитолог')
+
+insert into doctor_specialization (name )
+values ('паркинсонолог')
+
+insert into doctor_specialization (name )
+values ('пародонтолог')
+
+insert into doctor_specialization (name )
+values ('подолог')
+
+insert into doctor_specialization (name )
+values ('проктолог')
+
+insert into doctor_specialization (name )
+values ('психиатр')
+
+insert into doctor_specialization (name )
+values ('психолог')
+
+insert into doctor_specialization (name )
+values ('радиолог')
+
+insert into doctor_specialization (name )
+values ('реаниматолог')
+
+insert into doctor_specialization (name )
+values ('ревматолог')
+
+insert into doctor_specialization (name )
+values ('рентгенолог')
+
+insert into doctor_specialization (name )
+values ('репродуктолог')
+
+insert into doctor_specialization (name )
+values ('сексолог')
+
+insert into doctor_specialization (name )
+values ('сексопатолог')
+
+insert into doctor_specialization (name )
+values ('сомнолог')
+
+insert into doctor_specialization (name )
+values ('стоматолог')
+
+insert into doctor_specialization (name )
+values ('суггестолог')
+
+insert into doctor_specialization (name )
+values ('сурдолог')
+
+insert into doctor_specialization (name )
+values ('терапевт')
+
+insert into doctor_specialization (name )
+values ('токсиколог')
+
+insert into doctor_specialization (name )
+values ('травматолог')
+
+insert into doctor_specialization (name )
+values ('трихолог')
+
+insert into doctor_specialization (name )
+values ('уролог')
+
+insert into doctor_specialization (name )
+values ('фтизиатр')
+
+insert into doctor_specialization (name )
+values ('хирург')
+
+insert into doctor_specialization (name )
+values ('эмбриолог')
+
+insert into doctor_specialization (name )
+values ('эндодонт')
+
+insert into doctor_specialization (name )
+values ('эпидемиолог')
+
+insert into doctor_specialization (name )
+values ('эпилептолог')
