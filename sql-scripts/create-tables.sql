0caf9a78645e08c492eaf2d5c3f9fb26d286c603
Andrey <kouznts@ya.ru>
2020-01-26 14:34:31 +0400

    исправить скрипт

--- sql-scripts/create-tables.sql
+++ sql-scripts/create-tables.sql
@@ -1,32 +1,32 @@
 ﻿CREATE TABLE patient (
-  id BIGINT NOT NULL IDENTITY PRIMARY KEY,
-  forename VARCHAR(100) NOT NULL,
-  patronymic VARCHAR(100) NOT NULL,
-  surname VARCHAR(100) NOT NULL,
+  id BIGINT NOT NULL IDENTITY,
+  forename VARCHAR(50) NOT NULL,
+  patronymic VARCHAR(50) NOT NULL,
+  surname VARCHAR(50) NOT NULL,
   phone CHAR(11) NOT NULL
 );
 
 CREATE TABLE doctor (
-  id BIGINT NOT NULL IDENTITY PRIMARY KEY,
-  forename VARCHAR(100) NOT NULL,
-  patronymic VARCHAR(100) NOT NULL,
-  surname VARCHAR NOT(100) NULL,
+  id BIGINT NOT NULL IDENTITY,
+  forename VARCHAR(50) NOT NULL,
+  patronymic VARCHAR(50) NOT NULL,
+  surname VARCHAR(50) NULL,
   specialization_id BIGINT NOT NULL,
   creation_date DATETIME NOT NULL,
   validity_date DATETIME NOT NULL,
-  priority TINYINT(1) NOT NULL DEFAULT 3 CHECK(priority > 0 AND priority < 4),
+  priority TINYINT DEFAULT 3 NOT NULL CHECK(priority > 0 AND priority < 4)
 );
 
 CREATE TABLE doctor_specialization (
-  id BIGINT NOT NULL IDENTITY PRIMARY KEY,
-  name VARCHAR(100) NOT NULL,
+  id BIGINT NOT NULL IDENTITY,
+  name VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE medical_prescription (
-  id BIGINT NOT NULL IDENTITY PRIMARY KEY,
-  description TEXT NOT NULL,
+  id BIGINT NOT NULL IDENTITY,
+  description VARCHAR(500) NOT NULL,
   patient_id BIGINT NOT NULL,
-  doctor_id BIGINT NOT NULL,
+  doctor_id BIGINT NOT NULL
 );
 
 ALTER TABLE doctor ADD FOREIGN KEY (specialization_id) REFERENCES doctor_specialization (id);
