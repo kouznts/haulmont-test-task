b8f3f41f8bab58ace326cf35701a3db8c622b535
Andrey <kouznts@ya.ru>
2020-01-27 15:15:53 +0400

    запросы к "врачам"

--- sql-scripts/queries.sql
+++ sql-scripts/queries.sql
@@ -22,3 +22,32 @@ update patient set phone = '0123456789' where id = 0
 delete from patient where id = 0
 ------------------------------------------------------
 ------------------------------------------------------
+
+-- ВРАЧИ
+------------------------------------------------------
+-- отображение списка врачей
+select * from doctor
+
+-- добавление нового врача
+insert into doctor
+(forename, patronymic, surname, specialization_id)
+values ('ИМЯ', 'ОТЧЕСТВО', 'ФАМИЛИЯ', 0)
+
+-- редактирование врача
+-- имя (id, forename)
+update doctor set forename = 'Добрыня' where id = 0
+-- отчество (id, patronymic)
+update doctor set patronymic = 'Никитич' where id = 0
+-- фамилия (id, surname)
+update doctor set surname = 'Богатырёв' where id = 0
+-- специализация (id, doctor_specialization_id)
+update doctor set specialization_id = 0 where id = 0
+
+-- удаление пациента (id)
+delete from doctor where id = 0
+
+-- отображение статистической информации по количеству рецептов,
+-- выписанных врачами
+select * from medical_prescription where doctor_id = 0
+------------------------------------------------------
+------------------------------------------------------
