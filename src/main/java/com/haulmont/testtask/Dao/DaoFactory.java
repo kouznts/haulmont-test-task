4be1fb43b09f6c38e45a724f389e0edbc4de6f61
Andrey <kouznts@ya.ru>
2020-01-29 08:49:05 +0400

    HsqldbDao

--- src/main/java/com/haulmont/testtask/Dao/DaoFactory.java
+++ src/main/java/com/haulmont/testtask/Dao/DaoFactory.java
@@ -1,12 +1,12 @@
 package com.haulmont.testtask.Dao;
 
 public abstract class DaoFactory {
-    public enum DaoFactoryTypes {HSQLDB}
+    public enum DaoTypes {HSQLDB}
 
-    public static DaoFactory getDaoFactory(DaoFactoryTypes daoFactoryTypes) {
-        switch (daoFactoryTypes) {
+    public static HsqldbDao getDao(DaoTypes daoType, String connectionUrl) {
+        switch (daoType) {
             case HSQLDB:
-                return new HsqlDbDaoFactory();
+                return new HsqldbDao(connectionUrl);
             default:
                 return null;
         }
